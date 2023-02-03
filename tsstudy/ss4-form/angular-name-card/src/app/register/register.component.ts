76013e1fa15edf2ed96cff471f3c9e415bdfd0ab
htdat0704 <hodat402@gmail.com>
2023-02-03 16:58:24 +0700

    second commnit

--- tsstudy/ss4-form/angular-name-card/src/app/register/register.component.ts
+++ tsstudy/ss4-form/angular-name-card/src/app/register/register.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { Register } from '../register';
-import {FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import {FormControl, FormGroup, Validators, FormBuilder, AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
+// export const passValidation = (control: FormGroup): { [key: string]: boolean } | null => {
+//   const password = control.get('password');
+//   const confirmPassword = control.get('passwordConfirm');
+
+//   return password !== confirmPassword ? { isError: true } : null;
+// }
 
 export class RegisterComponent {
   register : Register = {
@@ -19,6 +27,7 @@ export class RegisterComponent {
   };
   error: string = "";
 
+  
   // registerForm! : FormGroup;
   // registerForm : FormGroup | undefined;
 
@@ -37,10 +46,31 @@ export class RegisterComponent {
     
   }
 
+  createPasswordStrengthValidator(): ValidatorFn {
+    return (control:AbstractControl) : ValidationErrors | null => {
+
+        const value = control.value;
+
+        if (!value) {
+            return null;
+        }
+
+        const hasUpperCase = /[A-Z]+/.test(value);
+
+        const hasLowerCase = /[a-z]+/.test(value);
+
+        const hasNumeric = /[0-9]+/.test(value);
+
+        const passwordValid = hasUpperCase && hasLowerCase && hasNumeric;
+
+        return !passwordValid ? {passwordStrength:true}: null;
+    }
+}
+
   registerForm =this.fb.group({
-    email: ["",[Validators.required,Validators.minLength(16)]],
-    password: "",
-    passwordConfirm: "",
+    email: ["",[Validators.required,Validators.email]],
+    password: ["",[Validators.required,Validators.minLength(6),this.createPasswordStrengthValidator]],
+    passwordConfirm: ["",[Validators.required,Validators.minLength(6),this.createPasswordStrengthValidator]],
     country: "",
     age: 0,
     gender: false,
